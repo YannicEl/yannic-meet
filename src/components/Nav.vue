536dcbc96961bc436d7a2cfac5a0604d8c4669f9
yannic.ellhotka@gmail.com <yannic.ellhotka@gmail.com>
2021-03-22 19:52:46 +0100

    updated nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,10 +1,6 @@
 <template>
-  <router-link to="/" v-slot="{ isActive }">
-    <span class="mr-4 hover:underline" :class="{ underline: isActive }">Home</span>
-  </router-link>
-  <router-link to="/about" v-slot="{ isActive }">
-    <span class="mr-4 hover:underline" :class="{ underline: isActive }">About</span>
-  </router-link>
+  <router-link class="mr-4 hover:underline" to="/" active-class="underline">Home</router-link>
+  <router-link class="mr-4 hover:underline" to="/about" active-class="underline">About</router-link>
 </template>
 
 <script lang="ts">
